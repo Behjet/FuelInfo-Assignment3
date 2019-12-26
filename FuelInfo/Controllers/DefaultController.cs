d46fe123a86ce3737e1bf31c0385f2fc34ae5c8d
Behjet <31242852+Behjet@users.noreply.github.com>
2019-12-26 15:38:44 +0500

    Complete HW3

---
+++ FuelInfo/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FuelInfo.Models;
+
+namespace FuelInfo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private ContextClass context = new ContextClass();
+        
+
+        public ActionResult Index()
+        {
+            
+            ContextClass context = new ContextClass();
+            var products = context.Categories.ToList();
+            products = products.Skip(Math.Max(0, products.Count() - 3)).ToList();
+            return View(products);
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Category());
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+    }
+}
---
+++ FuelInfo/Models/ContextClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FuelInfo.Models
+{
+    public class ContextClass : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
